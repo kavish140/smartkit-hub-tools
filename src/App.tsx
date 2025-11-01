7a5a5d5b027866c25066cce438349cac3f1a2ea9
Kavish Ganatra <kavishganatra5@gmail.com>
2025-11-01 15:41:17 +0530

    Scroll up glitch fix

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { useVisitTracking } from "@/hooks/useVisitTracking";
+import ScrollToTop from "@/components/ScrollToTop";
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
 import PasswordGenerator from "./pages/PasswordGenerator";
@@ -65,6 +66,7 @@ const App = () => {
       <Toaster />
       <Sonner />
       <BrowserRouter basename={import.meta.env.BASE_URL}>
+        <ScrollToTop />
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/calculator" element={<Calculator />} />
